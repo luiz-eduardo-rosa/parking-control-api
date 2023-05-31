ced74f247e4a03d56f90f772a1a5d0a43955be4d
Luiz Eduardo <luizeduardodu@hotmail.com>
2023-05-31 13:17:14 -0300

    Project

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PARKING-SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    
+}
