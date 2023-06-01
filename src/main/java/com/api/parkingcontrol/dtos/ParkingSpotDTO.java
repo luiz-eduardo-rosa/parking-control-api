4082da014a23a60d137cb66e549b5a80b2ee14c3
Luiz Eduardo <luizeduardodu@hotmail.com>
2023-06-01 15:59:41 -0300

    method POST

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDTO.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDTO.java
@@ -3,30 +3,23 @@ package com.api.parkingcontrol.dtos;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-public class ParkingSpotDTO {
-    
+public class ParkingSpotDto {
+
     @NotBlank
     private String parkingSpotNumber;
-    
     @NotBlank
     @Size(max = 7)
     private String licensePlateCar;
-    
     @NotBlank
     private String brandCar;
-    
     @NotBlank
     private String modelCar;
-    
     @NotBlank
     private String colorCar;
-    
     @NotBlank
     private String responsibleName;
-    
     @NotBlank
     private String apartment;
-    
     @NotBlank
     private String block;
 
@@ -93,6 +86,4 @@ public class ParkingSpotDTO {
     public void setBlock(String block) {
         this.block = block;
     }
-
-    
 }
